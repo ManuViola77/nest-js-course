69c826dbff8bdb7d5780f1271a3c22f9be1c0e6b
Manuela Viola <manuela.viola@gmail.com>
2025-05-13 00:56:08 -0300

    feat: signup

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,10 +2,21 @@ import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository {
   constructor(
     @InjectRepository(User) private readonly repository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.repository.create({
+      username,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
 }
