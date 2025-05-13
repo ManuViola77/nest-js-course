5a369874137823cd88fe849904f59a1c32770d27
Manuela Viola <manuela.viola@gmail.com>
2025-05-13 11:46:58 -0300

    feat: serialize user data, create interceptor to not include user data on the create task answer

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 import { TaskStatus } from './task-status.enum';
 import { User } from 'src/auth/user.entity';
 
@@ -17,5 +18,6 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne(() => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
